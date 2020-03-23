62e3d82befcac2086b4ac8919a6aaa20060e535f
Tim Chu <timchu92@gmail.com>
2020-03-23 11:56:42 -0400

    moved components into designated folder and indexed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import logo from "./logo.svg";
-import Home from "./Home.js";
-import Kits from "./Kits.js";
-import Chart from "./Chart.js";
+import { Home, Kits, Chart } from "./components";
 import "./App.css";
 
 class App extends Component {
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from "./Home.js";
+export { default as Kits } from "./Kits.js";
+export { default as Chart } from "./Chart.js";
