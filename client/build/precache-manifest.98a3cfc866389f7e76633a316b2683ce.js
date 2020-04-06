c924ae310c0b303329c7aee8e297ef6bfd07871a
Tim Chu <timchu92@gmail.com>
2020-04-05 22:36:09 -0400

    added build folder

---
+++ client/build/precache-manifest.98a3cfc866389f7e76633a316b2683ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073e969a3414ff5339d79a071c0184e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e8ee999884c95ae91d0",
+    "url": "/static/css/main.ed2d5806.chunk.css"
+  },
+  {
+    "revision": "7126e7f12e635202f8fa",
+    "url": "/static/js/2.9641b2ae.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.9641b2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8ee999884c95ae91d0",
+    "url": "/static/js/main.bc206755.chunk.js"
+  },
+  {
+    "revision": "390435d7c0d6f21ecd5f",
+    "url": "/static/js/runtime-main.fe77790f.js"
+  }
+]);
